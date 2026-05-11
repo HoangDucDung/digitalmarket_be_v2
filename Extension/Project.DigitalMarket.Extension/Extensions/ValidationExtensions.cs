2af6acd7e01dfa8fff0574e55c4b6ca8720e0357
Dung Hoang <hoangducdung201@gmail.com>
2026-05-11 14:30:32 +0700

    Check code

--- Extension/Project.DigitalMarket.Extension/Extensions/ValidationExtensions.cs
+++ Extension/Project.DigitalMarket.Extension/Extensions/ValidationExtensions.cs
@@ -1,10 +1,14 @@
-﻿namespace Project.Extensions.Extensions
+using System.Diagnostics.CodeAnalysis;
+
+namespace Project.Extensions.Extensions
 {
     public static class ValidationExtensions
     {
-        public static bool IsNullOrEmpty(this string? value) => string.IsNullOrEmpty(value);
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable) => enumerable == null || !enumerable.Any();
-        public static bool HasValue(this string? value) => !string.IsNullOrEmpty(value);
-
+        public static bool IsNullOrEmpty([NotNullWhen(false)] this string? value) => string.IsNullOrEmpty(value);
+        
+        public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable) => enumerable == null || !enumerable.Any();
+        
+        public static bool HasValue([NotNullWhen(true)] this string? value) => !string.IsNullOrEmpty(value);
     }
 }
+
